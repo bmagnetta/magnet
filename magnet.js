d228c1eb3f905d0074f7367fce7093c720b25ae5
bmagnetta <bmagnett@gmail.com>
2019-05-25 01:05:11 -0400

    photo scroll
    Completed auto generation of slide show, connecting to firebase database and sotrage! sucess!

--- magnet.js
+++ magnet.js
@@ -50,15 +50,7 @@ const storageRef = storageService.ref();
 
 //---------------------
 
-//prevent enter key submiting form
-$(document).ready(function() {
-  $(window).keydown(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      return false;
-    }
-  });
-});
+
 
 
 document.getElementById("rsvp-submit").addEventListener("click", function(){
@@ -107,43 +99,42 @@ function rsvpSubmitScript() {
 //-----------------------
 
 
-document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
-document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
+//document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
+//document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
 
 
 
 let selectedFile;
 function handleFileUploadChange(e) {
-selectedFile = e.target.files[0];
-document.getElementById('file-submit').innerText = 'Post';
-
+    selectedFile = e.target.files[0];
+    document.getElementById('file-submit').innerText = 'Post';
 }
 
 function handleFileUploadSubmit(e) {
-document.getElementById('file-submit').innerText = 'Posted';
+    document.getElementById('file-submit').innerText = 'Posted';
 
-var rn1 = Math.floor(Math.random() * 20);
-var rn2 = Math.floor(Math.random() * 20);
-var rn3 = Math.floor(Math.random() * 20);
-var rn4 = Math.floor(Math.random() * 20);
-var rn5 = Math.floor(Math.random() * 20);
+    var rn1 = Math.floor(Math.random() * 20);
+    var rn2 = Math.floor(Math.random() * 20);
+    var rn3 = Math.floor(Math.random() * 20);
+    var rn4 = Math.floor(Math.random() * 20);
+    var rn5 = Math.floor(Math.random() * 20);
 
-var rid = rn1.toString()+rn2.toString()+rn3.toString()+rn4.toString()+rn5.toString();
+    var rid = rn1.toString()+rn2.toString()+rn3.toString()+rn4.toString()+rn5.toString();
 
-const uploadTask = storageRef.child(`images/${rid+selectedFile.name}`).put(selectedFile); //create a child directory called images, and place the file inside this directory
-uploadTask.on('state_changed', (snapshot) => {
-// Observe state change events such as progress, pause, and resume
-}, (error) => {
-  // Handle unsuccessful uploads
-  console.log(error);
-}, () => {
-   // Do something once upload is complete
-   console.log('success');
-});
-// add file name to firebase database
-firebase.database().ref('photo_booth').push().set({
-  filename:rid+selectedFile.name
-});
+    const uploadTask = storageRef.child(`images/${rid+selectedFile.name}`).put(selectedFile); //create a child directory called images, and place the file inside this directory
+    uploadTask.on('state_changed', (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    }, (error) => {
+      // Handle unsuccessful uploads
+      console.log(error);
+    }, () => {
+       // Do something once upload is complete
+       console.log('success');
+    });
+    // add file name to firebase database
+    firebase.database().ref('photo_booth').push().set({
+      filename:rid+selectedFile.name
+    });
 
 
 };
@@ -202,31 +193,212 @@ getImageData(firebase,storageRef);
 //------------------------------
 
 
+//============================================================
+//
+//---Photo Slideshow
+//
+//============================================================
 
+//--------------------------------
+//---Create slideshow
+//--------------------------------
 
-var myIndex2 = 0;
-carouselSchedule("mySlides2");
+window.myIndex = 1;
+window.photoScrolls = {};
+window.photoSelected = {};
 
-function carouselSchedule() {
-  var i;
-  var x = document.getElementsByClassName("mySlides2");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex2++;
-  if (myIndex2 > x.length) {
-    //getImageData(firebase,storageRef)
-    myIndex2 = 1
-  }
-  x[myIndex2-1].style.display = "block";
-  setTimeout(carouselSchedule, 10000); // Change image every 2 seconds
+function BuildPhotoFeed(div_name) {
+    //div_name == id of element in website, path in database
+    //window.magnet_api_key, in setup instructions, included in code snippet.
+
+    window.photoScrolls[div_name]=1//set global index book keeping variable.
+    window.photoSelected[div_name]=""//set global structure for keep track of selected file for each feed.
+    
+    var i;
+    for (i = 1; i < 4; i++) {
+        var card_name = div_name+"-card"+i;
+
+        var card = document.createElement("DIV");
+        card.setAttribute("id", card_name);
+        card.setAttribute("class", div_name+" w3-row w3-padding-32 w3-card w3-center");
+        document.getElementById(div_name).appendChild(card);
+        
+        var img = document.createElement("IMG");
+        img.setAttribute("id", div_name+"-i"+i);
+        img.setAttribute("class", "w3-animate-fading w3-hover-opacity");
+        img.setAttribute("style", "width:100%");
+        pullImageData(div_name,div_name+"-i"+i)
+        document.getElementById(card_name).appendChild(img);
+
+    }
+    var select = document.createElement("INPUT");
+    select.setAttribute("type", "file");
+    select.setAttribute("class", "file-select");
+    select.setAttribute("accept", "image/*");
+    select.setAttribute("name", div_name);
+    select.addEventListener('change', handlePhotoUploadChange);
+    document.getElementById(div_name).appendChild(select);
+    
+    
+    var submit = document.createElement("BUTTON");
+    submit.setAttribute("id", "file-submit");
+    submit.setAttribute("class", "file-submit");
+    submit.setAttribute("name", div_name);
+    submit.innerText = '';
+    submit.addEventListener('click', handlePhotoUploadSubmit);
+    document.getElementById(div_name).appendChild(submit);
+    
 }
 
-//------------------------------
 
-function populateTest(div_name,input_names) {
+//https://stackoverflow.com/questions/30469755/why-a-variable-defined-global-is-undefined
+function StartPhotoScroll() {
+    //console.log(window.myIndex);
+    const keys = Object.keys(window.photoScrolls);
+
+    for (const key of keys) {
+        //window.photoScrolls[key] this is the index variable, key is ps_key
+        var i;
+        var x = document.getElementsByClassName(key);
+
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        };
+        window.photoScrolls[key]=window.photoScrolls[key]+1;
+        //console.log(window.photoScrolls[key]);
+        if (window.photoScrolls[key] > x.length) {
+            //getImageData(firebase,storageRef)
+            window.photoScrolls[key] = 1;
+        };
+        x[window.photoScrolls[key]-1].style.display = "block";
+        setTimeout(StartPhotoScroll, 10000); // Change image every 2 seconds
+
+    }
+
+}
+
+//----------------------------------------------
+
+//download photos that are already on storage and update the photo feed susing the window.photoScrolls
+
+//include button to upload photos
+
+function handlePhotoUploadChange(e) {
+    var div_name = e.target.name;
+    window.photoSelected[div_name] = e.target.files[0];
+    var x = document.getElementsByClassName("file-submit");
+    for (i = 0; i < x.length; i++) {
+        if (x[i].name == div_name){
+            x[i].innerText = 'Post';
+        }
+    };
+
+
+}
+
+function handlePhotoUploadSubmit(e) {
+    
+    //only upload if
+    if (e.target.innerText != 'Posted' && e.target.innerText != '') {
+    
+        var div_name = e.target.name;
+        var selectedFile = window.photoSelected[div_name];
+        e.target.innerText = 'Posted';
+
+        var rn1 = Math.floor(Math.random() * 20);
+        var rn2 = Math.floor(Math.random() * 20);
+        var rn3 = Math.floor(Math.random() * 20);
+        var rn4 = Math.floor(Math.random() * 20);
+        var rn5 = Math.floor(Math.random() * 20);
+
+        var rid = rn1.toString()+rn2.toString()+rn3.toString()+rn4.toString()+rn5.toString();
+
+        const uploadTask = storageRef.child(`${window.magnet_api_key}/${rid+selectedFile.name}`).put(selectedFile); //create a child directory called images, and place the file inside this directory
+        uploadTask.on('state_changed', (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        }, (error) => {
+          // Handle unsuccessful uploads
+          console.log(error);
+        }, () => {
+           // Do something once upload is complete
+           console.log('success');
+        });
+        // add file name to firebase database
+        firebase.database().ref("data/"+window.magnet_api_key+"/"+div_name).push().set({
+          filename:rid+selectedFile.name
+        });
+
+    }
+
+};
+
+//--------------------------------------------------
+
+
+
+function pullImageData(div_name,img_id) {
+    var ref = firebase.database().ref("data/"+window.magnet_api_key+"/"+div_name);
+    ref.on('value', function(snapshot) {
+      var photo_booth_filenames = [];
+      //loop through each push-id which are unkown values
+      snapshot.forEach(function(childSnapshot) {
+        // key = push-id which are unkown values
+        var key = childSnapshot.key;
+        // the actual filename str value
+        var filename = childSnapshot.child("filename").val();
+        //listen and populate the full filename array
+        photo_booth_filenames.push(filename);
+      });
+      //console.log("calling downloadRandomImage");
+      
+      
+      function downloadRandomImage(photo_booth_filenames,img_id) {
+        //console.log("calling downloadRandomImage");
+        storageRef.child(window.magnet_api_key+"/"+_.sample(photo_booth_filenames)).getDownloadURL().then(function(url) {
+          // This can be downloaded directly:
+          var xhr = new XMLHttpRequest();
+          xhr.responseType = 'blob';
+          xhr.onload = function(event) {
+            var blob = xhr.response;
+          };
+          xhr.open('GET', url);
+          xhr.send();
+        
+          // Or inserted into an <img> element:
+          var img = document.getElementById(img_id);
+          img.src = url;
+        }).catch(function(error) {
+          // Handle any errors
+          console.log("Error: downloadRandomImage")
+        });
+      }
+      
+      
+      downloadRandomImage(photo_booth_filenames,img_id)
+      
+    });
+
+}
+
+
+
+
+
+
+
+//============================================================
+//
+//---Forms
+//
+//============================================================
+
+//--------------------------------
+//---Create Forms
+//--------------------------------
+
+function BuildForm(div_name,input_names) {
     //div_name == id of element in website, path in database
-    //magnet_api_key, in setup instructions, included in code snippet.
+    //window.magnet_api_key, in setup instructions, included in code snippet.
 
     var card_name = div_name+"-card";
     var form_name = div_name+"-form";
@@ -268,7 +440,7 @@ function populateTest(div_name,input_names) {
         console.log("calling ready");
 
         if ($('#'+form_name).length > 0 ) {
-            SubmitTestValidate(div_name,form_name,input_names);
+            SubmitFormValidate(div_name,form_name,input_names);
         }
     });
 
@@ -276,30 +448,27 @@ function populateTest(div_name,input_names) {
 
 }
 
-function SubmitTestValidate(div_name,form_name,input_names) {
+function SubmitFormValidate(div_name,form_name,input_names) {
 
-    var api_ref = firebase.database().ref("api_key/"+magnet_api_key);
+    var api_ref = firebase.database().ref("api_key/"+window.magnet_api_key);
     api_ref.once('value', function(snapshot) {
       if (snapshot.exists()) {
-        console.log("magnet_api_key is valid");
-        SubmitTest(div_name,form_name,input_names)
+        console.log("window.magnet_api_key is valid");
+        SubmitForm(div_name,form_name,input_names)
       }
       else {
-        console.log("magnet_api_key is invalid");
+        console.log("window.magnet_api_key is invalid");
       }
 
     });
 
 }
 
-
-
-
-function SubmitTest(div_name,form_name,input_names) {
+function SubmitForm(div_name,form_name,input_names) {
     console.log("calling SubmitTest");
     
 
-    var b = firebase.database().ref("data/"+magnet_api_key+"/"+div_name);
+    var b = firebase.database().ref("data/"+window.magnet_api_key+"/"+div_name);
     //what is the below script doing? $("#rsvp-n") retrieves this element.
     //https://javascript.info/forms-submit explains form.submit()
     $('#'+form_name).submit(function() {
@@ -314,63 +483,80 @@ function SubmitTest(div_name,form_name,input_names) {
     // !1 prevents the form from moving position after submission
 }
 
+//prevent enter key submiting form
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
 
-//------Visualization----------------
 
-function VisualizationTestValidate(vis_title,vis_name,form_name) {
+//--------------------------------
+//---Visualize Forms
+//--------------------------------
+
+function VisualizeFormValidate(vis_title,vis_name,form_name) {
 
     document.getElementById(vis_title).innerHTML = form_name;
 
-    var api_ref = firebase.database().ref("api_key/"+magnet_api_key);
+    var api_ref = firebase.database().ref("api_key/"+window.magnet_api_key);
     api_ref.once('value', function(snapshot) {
       if (snapshot.exists()) {
-        console.log("magnet_api_key is valid");
-        VisualizationTest(vis_name,form_name)
+        console.log("window.magnet_api_key is valid");
+        VisualizeForm(vis_name,form_name)
       }
       else {
-        console.log("magnet_api_key is invalid");
+        console.log("window.magnet_api_key is invalid");
       }
 
     });
 
 }
 
-
-function VisualizationTest(vis_name,form_name) {
+function VisualizeForm(vis_name,form_name) {
     console.log("calling VisualizationTest");
     
-    var b = firebase.database().ref("data/"+magnet_api_key+"/"+form_name);
+    var b = firebase.database().ref("data/"+window.magnet_api_key+"/"+form_name);
     
     b.once('value', function(snapshot) {
 
         var txt = "";
-        txt += "<table border='1'>"
-        
+        txt += "<table class='w3-table w3-centered w3-striped'>"
+        var button_ids = [];
         snapshot.forEach(function(childSnapshot) {
             // key will be "ada" the first time and "alan" the second time
             var key = childSnapshot.key;
-            txt += "<tr>"+"<td>" + key + "</td>"+"</tr>";
+            button_ids.push(key);
+            txt += "<tr>"+"<td>" + key + "</td>"+"<td>" + "<button class='w3-button w3-hover-red'"+"id="+key+">remove</button>" + "</td>"+"</tr>";
             // childData will be the actual contents of the child
             var childData = childSnapshot.val();
             for (var key in childData){
                 txt += "<tr>"+"<td>" + key + "</td>"+"<td>" + childData[key] + "</td>"+"</tr>";
-
-
             }
-
-
 
 
         });
 
         txt += "</table>"
         
-        console.log(txt)
+        //console.log(txt)
         document.getElementById(vis_name).innerHTML = txt;
-
+        
+        
+        /*
+        for (var key in button_ids){
+            document.getElementById(key).addEventListener("click", function(){
+                
+                var c = firebase.database().ref("data/"+window.magnet_api_key+"/"+form_name+"/"+key);
+                c.remove();
+                
+            });
+        }
+        */
+        
     });
-
-
 }
-
 
