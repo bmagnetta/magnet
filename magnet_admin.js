5317c9c0048e22f9603b502edbac88bde6a9390e
bmagnetta <bmagnett@gmail.com>
2019-05-31 01:15:26 -0400

    Proof of concept admin website started
    Building a site that includes the basic connections for each required task. Auth, database path generation, etc ...

---
+++ magnet_admin.js
@@ -0,0 +1,23 @@
+
+
+
+//---------------------
+
+//firebase
+var config = {
+apiKey: "AIzaSyD8EYxSsouxIjlHEhbascyGmV4fbVGuArU",
+authDomain: "wedweb-67168.firebaseapp.com",
+databaseURL: "https://wedweb-67168.firebaseio.com",
+projectId: "wedweb-67168",
+storageBucket: "wedweb-67168.appspot.com",
+messagingSenderId: "1049950905472"
+};
+firebase.initializeApp(config);
+const storageService = firebase.storage();
+const storageRef = storageService.ref();
+
+
+
+//---------------------
+
+
